6ae8f88ef6611ff6950164cccf7e8508ef1ad42a
Seung Lee <shl7@uw.edu>
2022-07-13 14:31:32 -0700

    Add initial files

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      // some code here
+    }
+  }
+}
+
+/*
+Write a method that can handle the different combinations of plays and make sure to write specs for all possible combination of plays. Your method should take 2 inputs - one for each player's choice. It should return "Player 1" if the first input wins, "Player 2" if the second input wins, and "Draw" if nobody wins.
+
+Start by considering all possible inputs and outputs. Then choose the simplest input/output values and use them to write your first spec. Write all specs in English first, and then translate them into test methods and make them pass, one by one. Don't start coding until you've written your English specs.
+
+Make sure you include a README for this project and create a Git repository for it.
+
+extended
+Find a way to get the computer to randomly select a 'play' and make your console app into a one player game. We don't yet know how to test something that's random, so it's okay to skip writing a spec for that part.
+Or, expand the rules of your game to include 3 players. Make sure to write specs for that before coding. You must understand your goal clearly before you can figure out how to get there.
+*/
